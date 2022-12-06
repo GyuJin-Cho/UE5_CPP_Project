accd1583e41f8da5061b862ea6666d8a4921e3cd
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2022-12-06 21:37:48 +0900

    초기커밋

---
+++ Source/UE5_CPP_Project/UE5_CPP_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_CPP_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_CPP_Project, "UE5_CPP_Project" );
